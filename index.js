c4da64dc3c5d0d3c65e48bdcdb4f14eb5e1378c7
M-Usman-Nasir <usmannasir98@gmail.com>
2024-12-07 23:46:47 +0500

    before mvc

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 // Mock data for demonstration purposes
-const users = require("./MOCK_DATA.json");
+// const users = require("./MOCK_DATA.json");
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Connection
+mongoose.connect("mongodb://127.0.0.1:27017/01-project")
+ .then(() => console.log("MongoDB connected"))
+ .catch((err) => console.log("MongoDB connection error", err));
 
 // Schema
 const userSchema = new mongoose.Schema({
@@ -16,7 +21,9 @@ const userSchema = new mongoose.Schema({
   job_title: { type: String }, // job_title: String,
   ip_address: String,
   id: Number,
-});
+},
+{ timestamps: true }
+);
 
 // Model
 const User = mongoose.model("User", userSchema);
@@ -45,62 +52,73 @@ app.use(express.urlencoded({ extended: false }));
 // });
 
 // Routes
-app.get("/users", (req, res) => {
+app.get("/users", async (req, res) => {
+  const allDbUsers = await User.find({});
   const html = `
         <ul>
-            ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+            ${allDbUsers
+            .map((user) => `<li>${user.first_name} - ${user.last_name} - ${user.email}</li>`)
+            .join("")}
         </ul>
     `;
   return res.send(html);
 });
 
 // REST API
-app.get("/api/users", (req, res) => {
+app.get("/api/users", async (req, res) => {
+  const allDbUsers = await User.find({});
   // res.setHeader("x-Content-Type", "application/json");
   // res.setHeader("x-name", "Usman");
   // Always add "X" to custom headers
   // console.log(req.headers);
-  return res.json(users);
+  return res.json(allDbUsers);
 });
 
 // Correct usage of app.route() for chaining methods
 app
   .route("/api/users/:id")
-  .get((req, res) => {
-    const id = Number(req.params.id);
-    const user = users.find((user) => user.id === id);
+  .get( async (req, res) => {
+    const user = await User.findById(req.params.id);
+    // const id = Number(req.params.id);
+    // const user = users.find((user) => user.id === id);
     if (!user) {
       return res.status(404).json({ error: "User not found" });
     }
     return res.json(user);
   })
-  .patch((req, res) => {
+  .patch( async(req, res) => {
+    await User.findByIdAndUpdate(req.params.id, {last_name: "Usman Nasir"}
+      // req.body, {new: true}
+    )
     const id = Number(req.params.id);
-    const userIndex = users.findIndex((user) => user.id === id);
+    // const userIndex = users.findIndex((user) => user.id === id);
 
-    if (userIndex === -1) {
-      return res.status(404).json({ error: "User not found" });
-    }
+    return res.json({ status: "success" });
+    // if (userIndex === -1) {
+      // return res.status(404).json({ error: "User not found" });
+    // }
 
     // Update user
-    const updatedUser = { ...users[userIndex], ...req.body };
-    users[userIndex] = updatedUser;
+    // const updatedUser = { ...users[userIndex], ...req.body };
+    // users[userIndex] = updatedUser;
 
     // Save to file
-    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users, null, 2), (err) => {
-      if (err) {
-        return res.status(500).json({ error: "Failed to save user" });
-      }
-      res.json(updatedUser);
-    });
+    // fs.writeFile("./MOCK_DATA.json", JSON.stringify(users, null, 2), (err) => {
+    //   if (err) {
+    //     return res.status(500).json({ error: "Failed to save user" });
+    //   }
+    //   res.json(updatedUser);
+    // });
   })
-  .delete((req, res) => {
-    const id = Number(req.params.id);
-    const userIndex = users.findIndex((user) => user.id === id);
+  .delete( async(req, res) => {
+    await User.findByIdAndDelete(req.params.id);
+    // const id = Number(req.params.id);
+    // const userIndex = users.findIndex((user) => user.id === id);
+    return res.json({ status: "Success" });
 
-    if (userIndex === -1) {
-      return res.status(404).json({ error: "User not found" });
-    }
+    // if (userIndex === -1) {
+    //   return res.status(404).json({ error: "User not found" });
+    // }
 
     // Delete user
     const deletedUser = users.splice(userIndex, 1);
@@ -114,7 +132,7 @@ app
     });
   });
 
-app.post("/api/users", (req, res) => {
+app.post("/api/users", async (req, res) => {
   // Create a new user
   const body = req.body;
   if (
@@ -126,10 +144,22 @@ app.post("/api/users", (req, res) => {
   ) {
     return res.status(400).json({ error: "All fields are required..." });
   }
-  users.push({ ...body, id: users.length + 1 });
-  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-    return res.status(201).json({ status: "success", id: users.length });
+
+  const result = await User.create({
+    first_name: body.first_name,
+    last_name: body.last_name,
+    email: body.email,
+    gender: body.gender,
+    job_title: body.job_title,
   });
+
+  console.log("Result",result);
+  return res.status(201).json({ status: "success", id: result.id });
+  
+  // users.push({ ...body, id: users.length + 1 });
+  // fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+  //   return res.status(201).json({ status: "success", id: users.length });
+  // });
   // console.log("Body", body);
 });
 
